f1c1177a1adf7a47ffa07e35af0eadc4822c0ccc
Alissa Schlecht <schlecht220@gmail.com>
2017-03-21 16:58:13 +0000

    change layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,25 @@
 import React, { Component } from 'react';
 import '../stylesheets/App.css';
 import Video from './Video';
-import { Layout, Menu, Icon } from 'antd';
-const { Header, Content, Footer, Sider } = Layout;
+import { Layout } from 'antd';
+const { Header, Sider, Content } = Layout;
 
 class App extends Component {
   render() {
     return (
-
+    <Layout>
+      <Header>Header</Header>
       <Layout>
-        <Sider
-          breakpoint="lg"
-          collapsedWidth="0"
-          onCollapse={(collapsed, type) => { console.log(collapsed, type); }}
-        >
-          <div className="logo" />
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
-            <Menu.Item key="1">
-              <Icon type="user" />
-              <span className="nav-text">nav 1</span>
-            </Menu.Item>
-            <Menu.Item key="2">
-              <Icon type="video-camera" />
-              <span className="nav-text">nav 2</span>
-            </Menu.Item>
-            <Menu.Item key="3">
-              <Icon type="upload" />
-              <span className="nav-text">nav 3</span>
-            </Menu.Item>
-            <Menu.Item key="4">
-              <Icon type="user" />
-              <span className="nav-text">nav 4</span>
-            </Menu.Item>
-          </Menu>
-        </Sider>
-        <Layout>
-          <Header style={{ background: '#fff', padding: 0 }} >
-            Danceoke
-          </Header>
-            <div>
-              <Video />
-              <Video />
-              <Video />
-            </div>
-        </Layout>
+        <Sider>Sider</Sider>
+        <Content>
+          <div>
+            <Video />
+            <Video />
+            <Video />
+          </div>
+        </Content>
       </Layout>
+    </Layout>
     );
   }
 }
