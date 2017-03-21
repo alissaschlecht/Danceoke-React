83f67743098a098850f6961e6b9d733abd97d47b
Alissa Schlecht <schlecht220@gmail.com>
2017-03-21 15:33:54 +0000

    add antd

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
 import '../stylesheets/App.css';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col } from 'antd';
 
 
 class Video extends Component {
   render() {
     return (
       <div className="videoContainer">
-        <Row className="videoRow">
-          <Col xs={12} md={4} >{'one'}</Col>
-          <Col xs={12} md={4} >{'two'} </Col>
-          <Col xs={12} md={4} >{'three'} </Col>
+        <Row  type="flex" justify="space-around">
+          <Col span={6} >{'one'}</Col>
+          <Col span={6} >{'two'} </Col>
+          <Col span={6} >{'three'} </Col>
         </Row>
       </div>
     );
   }
 }
-
 
 export default Video;
