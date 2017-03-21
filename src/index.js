8316f0bfc08cb26f3873a1c02f35b98f21b6787b
Cathleen Ghenne <cathleen@workangel.com>
2017-03-21 17:46:07 +0000

    test with antd

--- src/index.js
+++ src/index.js
@@ -2,13 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import './stylesheets/index.css';
-import 'antd';
-// import 'bootstrap/dist/css/bootstrap.css';
-// import 'bootstrap/dist/css/bootstrap-theme.css';
-// Put any other imports below so that CSS from your
-// components takes precedence over default styles.
 
 ReactDOM.render(
-  <App />, 
+  <App />,
   document.getElementById('root')
 );
